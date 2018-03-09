ff3c6774cfc4879cb83672392c240cfa35988de9
Will Hawkins <whh8b@virginia.edu>
2018-03-09 17:55:10 -0500

    Update msr_test.

--- Makefile
+++ Makefile
@@ -4,5 +4,7 @@ libmsr.so: msr.cpp msr.hpp Makefile
 	g++ -shared -fPIC msr.cpp -o libmsr.so $(CXXFLAGS)
 msr_test: libmsr.so msr_test.cpp
 	g++ -o msr_test msr_test.cpp -lmsr $(CXXFLAGS) -L.
+run_tests: msr_test
+	sudo LD_LIBRARY_PATH=`pwd`:$$LD_LIBRARY_PATH ./msr_test
 clean:
 	rm -f libmsr.so msr_test *.o
--- msr_test.cpp
+++ msr_test.cpp
@@ -3,33 +3,67 @@
 #include <iostream>
 #include <bitset>
 
-int main() {
+/*
+ * Put dashes after every 8 bits in the bit set.
+ */
+std::ostream &operator<<(std::ostream &os, std::bitset<8*sizeof(uint64_t)> bits)
+{
+	for (int i = (8*sizeof(uint64_t))-1; i>=0; i--) {
+		os << bits[i];
+		if (!(i%8))
+			os << "-";
+	}
+	os << std::endl;
+	return os;
+}
+
+int main()
+{
 	Msr msr;
 	uint64_t result = 0;
 	std::bitset<8*sizeof(uint64_t)> result_bits;
-	assert(msr.Read(0x10, result, 0) == 0);
+
+	/*
+	 * Read the TSC and print it.
+	 */
+	assert(msr.Read(MSR::MSR_IA32_TIME_STAMP_COUNTER, result, 0) == 0);
 	std::cout << "TSC: 0x" << std::hex << result << std::endl;
 
-	assert(msr.Read(0x38F, result, 0) == 0);
 	/*
-	std::cout << "MSR_CORE_PERF_GLOBAL_CTRL: 0x";
-
-	result_bits = std::bitset<8*sizeof(uint64_t)>(result);
-	for (int i = (8*sizeof(uint64_t))-1; i>=0; i--) {
-		std::cout << result_bits[i];
-		if (!(i%8))
-			std::cout << "-"; 
-	}
-	std::cout << std::endl;
-	*/
+	 * Read the global performance counter control register.
+	 */
+	assert(msr.Read(MSR::MSR_CORE_PERF_GLOBAL_CTRL, result, 0) == 0);
 	result_bits = std::bitset<8*sizeof(uint64_t)>(result);
 	std::cout << "MSR_CORE_PERF_GLOBAL_CTRL: 0x" << result_bits << std::endl;
 
+	/*
+	 * Write to the global performance counter control register.
+	 */
 	result = 0x70000000f;
-	//result = 0x0;
-	assert(msr.Write(0x38F, result, 0) == 0);
-	assert(msr.Read(0x38F, result, 0) == 0);
+	assert(msr.Write(MSR::MSR_CORE_PERF_GLOBAL_CTRL, result, 0) == 0);
+
+	/*
+	 * Read from the global performance counter control register
+	 * to verify that the write worked properly.
+	 */
+	assert(msr.Read(MSR::MSR_CORE_PERF_GLOBAL_CTRL, result, 0) == 0);
 	result_bits = std::bitset<8*sizeof(uint64_t)>(result);
 	std::cout << "MSR_CORE_PERF_GLOBAL_CTRL: 0x" << result_bits << std::endl;
+
+	assert(msr.Read(MSR::MSR_IA32_PERFEVTSEL0, result, 0) == 0);
+	result_bits = std::bitset<8*sizeof(uint64_t)>(result);
+	std::cout << "MSR_IA32_PERFEVTSEL0: 0x" << result_bits << std::endl;
+
+	result = MSR::EVT_BR_MISP_RETIRED |
+	         MSR::MASK_BR_MISP_RETIRED_ALL |
+	         MSR::EVENT_ENABLE |
+	         MSR::EVENT_USER_ENABLE;
+	result_bits = std::bitset<8*sizeof(uint64_t)>(result);
+	std::cout << "Result: 0x" << result_bits << std::endl;
+
+	assert(msr.Write(MSR::MSR_IA32_PERFEVTSEL0, result, 0) == 0);
+	assert(msr.Read(MSR::MSR_IA32_PERFEVTSEL0, result, 0) == 0);
+	result_bits = std::bitset<8*sizeof(uint64_t)>(result);
+	std::cout << "MSR_IA32_PERFEVTSEL0: 0x" << result_bits << std::endl;
 	return 0;
 }
